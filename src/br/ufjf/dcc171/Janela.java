438d141125f90288828023420edb3107ea3908bf
Igor Knop <igorknop@gmail.com>
2017-08-10 22:03:46 -0300

    Adiciona dois labels

--- src/br/ufjf/dcc171/Janela.java
+++ src/br/ufjf/dcc171/Janela.java
@@ -1,12 +1,20 @@
 package br.ufjf.dcc171;
 
+import java.awt.FlowLayout;
 import java.awt.HeadlessException;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class Janela extends JFrame{
 
     public Janela() throws HeadlessException {
         super("Exemplo Janela");
+        setLayout(new FlowLayout());
+        JLabel etiqueta = new JLabel("Universidade Federal de Juiz de Fora!");
+        add(etiqueta);
+        
+        JLabel etiqueta2 = new JLabel("Departamento de Ciência da Computação");
+        add(etiqueta2);
     }
     
     
