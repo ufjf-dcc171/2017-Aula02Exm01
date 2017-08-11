15e7aaaf0815f29d27c879d90b6b63047d048c9f
Igor Knop <igorknop@gmail.com>
2017-08-10 21:51:46 -0300

    Cria uma janela simples

---
+++ src/br/ufjf/dcc171/Aula02Exm01.java
@@ -0,0 +1,15 @@
+package br.ufjf.dcc171;
+
+import javax.swing.JFrame;
+
+public class Aula02Exm01 {
+
+    public static void main(String[] args) {
+        Janela janela = new Janela();
+        janela.setSize(320, 200);
+        janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        janela.setLocationRelativeTo(null);
+        janela.setVisible(true);
+    }
+    
+}
---
+++ src/br/ufjf/dcc171/Janela.java
@@ -0,0 +1,13 @@
+package br.ufjf.dcc171;
+
+import java.awt.HeadlessException;
+import javax.swing.JFrame;
+
+public class Janela extends JFrame{
+
+    public Janela() throws HeadlessException {
+        super("Exemplo Janela");
+    }
+    
+    
+}
